4ba468b1fc7d74a8a702eaae7eb64554185ae6e5
Antoine Carbonnel <antoinecarbonnel@yahoo.fr>
2021-12-13 10:33:32 +0100

    That last commit message about silly mistakes pales in comparision to this one

--- client/App.js
+++ client/App.js
@@ -1,56 +1,102 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, Button, View, TextInput, FlatList, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, Button, View, TextInput, FlatList, Dimensions, SafeAreaView} from 'react-native';
+const axios = require('axios')
 
 export default function App() {
-    const [username, setUsername] = useState("zerckel");
-    const [user, setUser] = useState({});
+  const [username, setUsername] = useState("zerckel");
+  const [user, setUser] = useState({});
 
-    function search() {
-        fetch(`http://localhost:4242/api/users/${username}`)
-            .then(res => res.json())
-            .then(response => setUser(response.user))
-    }
+  function search() {
+    axios.get(`http://10.23.100.84:4242/api/users/${username}`)
+            .then(response => setUser(response.data))
+  }
 
-    return (<View style={styles.container}>
-        <TextInput
-            onChangeText={setUsername}
-            value={username}
-        />
+  return (
+          <SafeAreaView style={{flex: 1}}>
+            <View style={styles.container}>
+              <View style={styles.header}>
+                <TextInput
+                        style={styles.input}
+                        onChangeText={setUsername}
+                        value={username}
+                />
 
-        <Button
-            onPress={search}
-            title="Search"
-            color="#841584"
-        />
-
-        <View style={styles.container}>
-            <FlatList
-                data={Object.keys(user)}
-                renderItem={({item}) => {
-                    return (
-                    <View style={styles.item}>
-                        <Text>
-                            {item}:
-                        </Text>
-                        <Text>
-                            {user[item]}
-                        </Text>
-                    </View>)
-                }}
-            />
-        </View>
-    </View>);
+                <Button
+                        onPress={search}
+                        styles={styles.button}
+                        title="Search"
+                        color="#3700B3"
+                />
+              </View>
+              <View style={styles.profile}>
+                <FlatList
+                        data={Object.keys(user)}
+                        keyExtractor={(item) => item}
+                        numColumns={false}
+                        renderItem={({item}) => {
+                          return (
+                                  <View key={item} style={styles.card}>
+                                    <Text style={styles.title}>
+                                      {item}:
+                                    </Text>
+                                    <Text style={styles.content}>
+                                      {user[item]}
+                                    </Text>
+                                  </View>)
+                        }}
+                />
+              </View>
+            </View>
+          </SafeAreaView>
+  );
 }
 
+const windowWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 22
+  container: {
+    width: windowWidth,
+    marginTop: 30,
+    flex: 1,
+    padding: 10,
+  },
+  header: {
+    flexDirection: "column",
+  },
+  input: {
+    marginVertical: 10,
+    backgroundColor: "#D3D3D3",
+    borderRadius: 5,
+    textAlign: "center",
+  },
+  button: {
+    borderRadius: 5,
+  },
+  profile: {
+    paddingVertical: 20,
+    alignItems: "center"
+  },
+  card: {
+    margin: 5,
+    padding: 10,
+    backgroundColor: '#fff',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    borderRadius: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 10,
+      height: 5,
     },
-    item: {
-        flexDirection: "row",
-        padding: 10,
-        fontSize: 18,
-        height: 44,
-    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+
+    elevation: 5,
+  },
+  title: {
+    fontSize: 20,
+    textTransform: "uppercase",
+    marginRight: 10
+  }
 });
